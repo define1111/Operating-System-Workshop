ac5154540aa215b27826cf4402677832e4ce22bc
define1111 <s.vershkov.def@yandex.ru>
2020-06-06 14:15:17 +0500

    refactoring task-0

--- task-0/bits.c
+++ task-0/bits.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+
+typedef __int32_t int32_t;
 
 enum { byte_size = 8 };
+const int32_t one_32bit = 1;
 
-__int8_t check_bit(__int32_t value, size_t position)
+int check_bit(int32_t value, size_t position)
 {
-    const static __int32_t one_32bit = 1;
     return !!(value & (one_32bit << position));
 }
 
-void println_bits(__int32_t value)
+void println_bits(int32_t value)
 {
-    size_t i = byte_size * sizeof(__int32_t) - 1;
+    size_t i = byte_size * sizeof(int32_t) - 1;
 
     while (i --> 0)
     {
@@ -23,10 +26,10 @@ void println_bits(__int32_t value)
 
 void loop()
 {
-    __int32_t num = 0;
-    __int32_t scanf_return = 0;
+    int32_t num = 0;
+    int32_t scanf_return = 0;
 
-    for(;;)
+    while (1)
     {
         fputs(">>> ", stdout);
         scanf_return = scanf("%d", &num);
